cae6dac2fa7ddc061ce7712955cd5e82d360ae19
AgnesMayYao <agnes.may.yao@gmail.com>
2016-11-21 13:01:51 -0500

    Add files via upload
    Can't upload all files, more than 100 files (Because of Google VR library)
    After compression, more than 25MB
    So uploaded the major files here

---
+++ Scripts/dragDropObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragDropObject : MonoBehaviour,dragDropObjectHandler {
+	private bool isHeld;
+	private GameObject reticle;
+	// Use this for initialization
+	void Start () {
+		isHeld = false;
+		reticle = GameObject.Find ("modifiedReticle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isHeld) {
+			Ray ray = new Ray (reticle.transform.position, reticle.transform.forward);
+			transform.position = ray.GetPoint (4);
+		}
+	}
+
+	public void handleTriggerStart (){
+		isHeld = true;
+	}
+	public void handleTriggerEnd(){
+		isHeld = false;
+	}
+
+}
---
+++ Scripts/dragDropObjectHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public interface dragDropObjectHandler : IEventSystemHandler {
+
+		void handleTriggerStart ();
+		void handleTriggerEnd();
+}
